e0e4f10137edadc7e513b279f36ff8beacf87c5d
HellEaglee <helleagleee@gmail.com>
2024-04-08 23:23:40 +0300

    Rename.js has been successfully created.

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,35 @@
+import { promises as fs } from "fs";
 const rename = async () => {
-    // Write your code here 
+  const sourcePath =
+    "D:/Trash/MITSO/Course 3/Semester 2/RICE/mitso-nodejs-basic/src/fs/files/wrongFilename.txt";
+  const destinationPath =
+    "D:/Trash/MITSO/Course 3/Semester 2/RICE/mitso-nodejs-basic/src/fs/files/properFilename.md";
+
+  try {
+    await fs.access(sourcePath);
+
+    try {
+      await fs.access(destinationPath);
+      throw new Error(
+        "FS operation failed: File properFilename.md already exists"
+      );
+    } catch (error) {
+      if (error.code !== "ENOENT") {
+        throw error;
+      }
+    }
+
+    await fs.rename(sourcePath, destinationPath);
+    console.log("File renamed successfully.");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error(
+        "FS operation failed: File wrongFilename.txt does not exist"
+      );
+    } else {
+      throw error;
+    }
+  }
 };
 
 await rename();
