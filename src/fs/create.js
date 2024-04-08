50c635b044c3f9ec9a8cd61904451bb403ac7f08
HellEaglee <helleagleee@gmail.com>
2024-04-08 22:56:29 +0300

    Create.js has been successfully created.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
 const create = async () => {
-    // Write your code here 
+  const folderPath = "./src/fs/files";
+  const filePath = `${folderPath}/fresh.txt`;
+  const fileContent = "I am fresh and young";
+  try {
+    const isFileExists = fs.existsSync(filePath);
+    if (isFileExists) {
+      throw new Error("FS operation failed: File already exists");
+    }
+    fs.writeFileSync(filePath, fileContent);
+    console.log("File created successfully");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
-
 await create();
