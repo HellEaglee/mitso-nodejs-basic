4c25c388301921bbd876c49137de947bc46d5626
HellEaglee <helleagleee@gmail.com>
2024-04-09 09:55:33 +0300

    Zip file has been successfully done.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const filePath =
+    "D:/Trash/MITSO/Course 3/Semester 2/RICE/mitso-nodejs-basic/src/zip/files/fileToCompress.txt";
+  const readStream = fs.createReadStream(filePath);
+  const writeStream = fs.createWriteStream("archive.gz");
+  const gzip = zlib.createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  await new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
+
+  await fs.promises.unlink(filePath);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const inputPath =
+    "D:/Trash/MITSO/Course 3/Semester 2/RICE/mitso-nodejs-basic/archive.gz";
+  const outputPath =
+    "D:/Trash/MITSO/Course 3/Semester 2/RICE/mitso-nodejs-basic/src/zip/files/fileToCompress.txt";
+
+  const readStream = fs.createReadStream(inputPath);
+  const writeStream = fs.createWriteStream(outputPath);
+  const gunzip = zlib.createGunzip();
+
+  readStream.pipe(gunzip).pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
 };
 
 await decompress();
